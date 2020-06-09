0c84fc40e7066805841101ec08bb54c5df425799
OroArmor <eliorona@live.com>
2020-06-09 11:16:56 -0700

    Abstracted out the reporting of numberconfig

--- src/main/java/com/robototes/logging/shuffleboard/configurables/abst/NumberConfig.java
+++ src/main/java/com/robototes/logging/shuffleboard/configurables/abst/NumberConfig.java
@@ -1,15 +1,10 @@
 package com.robototes.logging.shuffleboard.configurables.abst;
 
-import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import com.robototes.logging.shuffleboard.IConfigurable;
-import com.robototes.logging.shuffleboard.ShuffleBoardManager;
-
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
+import com.robototes.logging.shuffleboard.reporters.abst.NumberReporter;
 
 /**
  * This is a template class for all the number widgets
@@ -18,19 +13,10 @@ import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
  *
  * @param <S> The type of the widget
  */
-@SuppressWarnings("unchecked")
-public abstract class NumberConfig<S extends NumberConfig<S>> implements IConfigurable<Double, S> {
+public abstract class NumberConfig<S extends NumberConfig<S>> extends NumberReporter<S>
+		implements IConfigurable<Double, S> {
 
-	private final Supplier<Double> getter;
 	private final Consumer<Double> setter;
-	private final String name;
-	private final String tabName;
-
-	private boolean built = false;
-	private Map<String, Object> allProperties;
-	private SimpleWidget widget;
-
-	private NetworkTableEntry entry;
 
 	/**
 	 * Creates a new {@link NumberConfig}
@@ -41,31 +27,13 @@ public abstract class NumberConfig<S extends NumberConfig<S>> implements IConfig
 	 * @param tabName The tab for the widget
 	 */
 	public NumberConfig(Supplier<Double> getter, Consumer<Double> setter, String name, String tabName) {
-		this.getter = getter;
+		super(getter, name, tabName);
 		this.setter = setter;
-		this.name = name;
-		this.tabName = tabName;
-
-		widget = Shuffleboard.getTab(getTabName()).add(getName(), getter).withWidget(getType());
-	}
-
-	@Override
-	public Supplier<Double> getReporter() {
-		return getter;
 	}
 
 	@Override
 	public Consumer<Double> getSetter() {
 		return setter;
-	}
-
-	/**
-	 * Initializes this against {@link ShuffleBoardManager}
-	 */
-	public void initializeWidget() {
-		ShuffleBoardManager.getInstance().add(this);
-		entry = widget.withProperties(allProperties).getEntry();
-		pastGet = getter.get();
 	}
 
 	private double pastGet;
@@ -75,60 +43,10 @@ public abstract class NumberConfig<S extends NumberConfig<S>> implements IConfig
 		double value = entry.getDouble(Double.NaN);
 
 		if (value == pastGet) {
-			double newValue = getter.get();
-			entry.setDouble(newValue);
-			pastGet = newValue;
+			super.update();
 		} else if (value != Double.NaN) {
 			setter.accept(value);
 		}
-	}
-
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public String getTabName() {
-		return tabName;
-	}
-
-	@Override
-	public S withSize(int width, int height) {
-		checkNotBuilt();
-		widget.withSize(width, height);
-		return (S) this;
-	}
-
-	@Override
-	public S withPosition(int columnIndex, int rowIndex) {
-		checkNotBuilt();
-		widget.withPosition(columnIndex, rowIndex);
-		return (S) this;
-	}
-
-	@Override
-	public S withProperties(Map<String, Object> properties) {
-		checkNotBuilt();
-		if (this.allProperties == null) {
-			allProperties = properties;
-		} else {
-			allProperties.putAll(properties);
-		}
-		return (S) this;
-	}
-
-	@Override
-	public S build() {
-		checkNotBuilt();
-		initializeWidget();
-		built = true;
-		return (S) this;
-	}
-
-	@Override
-	public boolean hasBeenBuilt() {
-		return built;
 	}
 
 }
