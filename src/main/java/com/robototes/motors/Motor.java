a0e295d87be5d2f31d60ad3ee777b7fa2d834194
SumedhP <usa1998dec@gmail.com>
2020-07-20 20:14:46 -0700

    added wpiapi to get WPI_Talon to work

--- src/main/java/com/robototes/motors/Motor.java
+++ src/main/java/com/robototes/motors/Motor.java
@@ -1,5 +1,21 @@
 package com.robototes.motors;
 
 public interface Motor {
+	
+	public void set(double speed);
+	
+	public double get();
+	
+	public void setInverted(boolean inverted);
+	
+	public boolean getInverted();
+	
+	public void setP();
+	
+	public void setI();
+	
+	public void setD();
+	
+	public void setF();
 
 }
--- src/main/java/com/robototes/motors/RobototesTalonFX.java
+++ src/main/java/com/robototes/motors/RobototesTalonFX.java
@@ -1,12 +1,49 @@
 package com.robototes.motors;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-public class RobototesTalonFX extends TalonFX {
+public class RobototesTalonFX extends WPI_TalonFX implements Motor {
 
 	public RobototesTalonFX(int channel) {
 		super(channel);
 	}
+
+	@Override
+	public void set(double speed) {
+		super.set(ControlMode.PercentOutput, speed);
+	}
+
+	@Override
+	public double get() {
+		return super.get();
+	}
+
+	@Override
+	public void setP() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setD() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setF() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 
 }
