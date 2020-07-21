c7d0ac017ca608ada22afbdc819e4003559f8821
SumedhP <usa1998dec@gmail.com>
2020-07-20 18:17:06 -0700

    Added CTRE files

---
+++ src/main/java/com/robototes/motors/Motor.java
@@ -0,0 +1,5 @@
+package com.robototes.motors;
+
+public interface Motor {
+
+}
---
+++ src/main/java/com/robototes/motors/RobototesTalonFX.java
@@ -0,0 +1,12 @@
+package com.robototes.motors;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class RobototesTalonFX extends TalonFX {
+
+	public RobototesTalonFX(int channel) {
+		super(channel);
+	}
+
+
+}
